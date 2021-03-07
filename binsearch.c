96f26794796bc3309cdf918ed99d21a2512c0d91
zhou-xiao-kun <59337034+zhou-xiao-kun@users.noreply.github.com>
2021-03-07 21:47:38 +0800

    Update binsearch.c

--- binsearch.c
+++ binsearch.c
@@ -6,9 +6,13 @@
 int main()
 {
     int array[] = {1,2,3,4,5,6,7,8};
+    /* 数组的元素的个数 */
     int size = sizeof(array)/sizeof(array[0]);
+    /* 最小的查找范围的索引值 */
     int minSubscript = 0;
+    /* 最大的查找范围的索引值 */
     int maxSubscript = size - 1;
+    /* 想要查找的数值 */
     int wantFind = 7;
     while(minSubscript<=maxSubscript)
     {
